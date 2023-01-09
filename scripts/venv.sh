3a58ab812e45805d08d01b4ec10a6003473bbdeb
Lars Sætaberget <2572988+Lars-Saetaberget@users.noreply.github.com>
2023-01-09 13:53:59 +0100

    Rename venv

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/echo This script needs to be sourced, not run directly: source
 
+venv_name=".venv"
+
 venv_search() {
 	current=$PWD
 	while [[ -n "$current" ]] ; do
-		result=$(find "$current" -maxdepth 1 -type d -name venv)
+		result=$(find "$current" -maxdepth 1 -type d -name "$venv_name")
 		if [[ -n $result ]]; then
 			return 0;
 		fi
@@ -19,7 +21,7 @@ elif venv_search; then
 	relpath=$(realpath --relative-to='.' "$result")
 	dir_name="\e[1m$(echo -n "$result" | rev | cut -f 2 -d '/' | rev)\e[0m"
 	string="Activating existing venv for $dir_name"
-	if [[ $relpath != "venv" ]]; then
+	if [[ $relpath != "$venv_name" ]]; then
 		string="$string ( $relpath )"
 	fi
 	echo -e "$string"
@@ -30,8 +32,8 @@ else
 	if [[ "$ans" != N* && "$ans" != n* ]]; then
 		echo "Creating new venv"
 		prompt=$(realpath . | rev | cut -f 1 -d '/' | rev)
-		python3 -m venv venv --prompt "\e[96m$prompt\e[0m"
-		source venv/bin/activate
+		python3 -m venv "$venv_name" --prompt "\e[96m$prompt\e[0m"
+		source "$venv_name"/bin/activate
 
 		if ls requirements.txt > /dev/null 2> /dev/null; then
 			echo -n "Install requirements in new venv? (Y/n): "
