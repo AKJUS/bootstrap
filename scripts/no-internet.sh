b3c235e414d383402da3adb22ead2275af785f46
Lars Sætaberget <2572988+Lars-Saetaberget@users.noreply.github.com>
2023-01-11 11:13:34 +0100

    Check for existence of no-internet group before running command

--- scripts/no-internet.sh
+++ scripts/no-internet.sh
@@ -5,6 +5,11 @@
 # Causes a slight delay in command execution while testing for internet access
 #
 
+if ! sg no-internet : &> /dev/null; then
+	echo "${0##*/}: Unable to run commands as no-internet group - ABORT!"
+	exit 1
+fi
+
 if sg no-internet "ping -W 0.05 -c 1 8.8.8.8" &> /dev/null; then
 	echo "${0##*/}: Internet connection detected - ABORT!"
 	exit 1
